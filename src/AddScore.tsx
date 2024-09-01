cb02cb28020cf150f57b9c12757dea238f7ccb9a
wangyk19 <wangyk19@lenovo.com>
2024-09-02 01:19:41 +0800

    fix: fix code

--- src/AddScore.tsx
+++ src/AddScore.tsx
@@ -1,10 +1,4 @@
-import React, {
-  forwardRef,
-  useImperativeHandle,
-  useRef,
-  useState,
-} from "react";
-import { Select, Form, Button, Space, Modal, Transfer, Tooltip } from "antd";
+import { forwardRef, useImperativeHandle, useRef, useState } from "react";
 import { produce } from "immer";
 
 import {
